2bbab32062aa7fb4b9c525d4cc396082cc5fb53e
svsem <svyasem@gmail.com>
2020-05-10 11:59:46 +0300

    Refactoring

--- src/components/VoronoiDiagram.vue
+++ src/components/VoronoiDiagram.vue
@@ -17,37 +17,18 @@
   </svg>
 </template>
 <script>
-import { reactive, computed } from 'vue'
+import { reactive, computed, watchEffect, ref } from 'vue'
 import PoissonDisk from 'poisson-disk-sampling'
 import { Delaunay } from 'd3-delaunay'
 import random from 'random'
-import cloneDeep from 'lodash/cloneDeep'
 import { useAnimationFrame } from '../logic/useAnimationFrame'
 import { Color, Gradient } from '../utils'
-
-function createParticleView(data, i) {
-  const result = {}
-
-  const genGetterAndSetter = (propertyName, arrayName) =>
-    Object.defineProperty(result, propertyName, {
-      get: () => data[arrayName][i],
-      set: value => data[arrayName][i] = value,
-    })
-
-  genGetterAndSetter('animation', 'animations')
-  genGetterAndSetter('color', 'colors')
-  genGetterAndSetter('originalColor', 'originalColors')
-  genGetterAndSetter('position', 'positions')
-  genGetterAndSetter('velocity', 'velocities')
-
-  return result
-}
 
 const genParticles = (boundsSizeX, boundsSizeY) => {
   const poissonDisk = new PoissonDisk({
         shape: [boundsSizeX, boundsSizeY],
         minDistance: 100,
-        maxDistance: 300,
+        maxDistance: 150,
         tries: 30,
       })
   const positions = poissonDisk.fill()
@@ -65,84 +46,74 @@ const genParticles = (boundsSizeX, boundsSizeY) => {
     return '#' + hex + hex + hex
   }
 
-  const colors = positions.map((_, i) => genParticleColor(i))
-
-  const data = reactive({
-    positions,
-    velocities: positions.map(() => genParticleVelocity()),
-    originalColors: colors,
-    colors: cloneDeep(colors),
-    animations: [],
+  return positions.map((position, i) => {
+    const color = genParticleColor(i)
+    return {
+      position,
+      velocity: genParticleVelocity(),
+      color,
+      originalColor: color,
+      animation: undefined,
+    }
   })
-
-  return {
-    data,
-    particles: positions.map((_, i) => createParticleView(data, i)),
-  }
 }
 
-const genCells = (positions, colors, boundsSizeX, boundsSizeY) => {
+const genCells = (particles, boundsSizeX, boundsSizeY) => {
   const voronoi =
     Delaunay
-    .from(positions)
+    .from(particles.map(p => p.position))
     .voronoi([0, 0, boundsSizeX, boundsSizeY])
 
-  return positions.map((_, i) => ({
+  return particles.map(({ color }, i) => ({
     path: voronoi.renderCell(i),
-    color: colors[i],
+    color,
   }))
 }
 
 export default {
   name: 'VoronoiDiagram',
   setup() {
-    const boundsSizeX = window.innerWidth
-    const boundsSizeY = window.innerHeight
-
-    const { particles, data: particlesData } = genParticles(boundsSizeX, boundsSizeY)
-
     const state = reactive({
-      particles,
-      cells: [],
-      boundsSizeX,
-      boundsSizeY,
+      boundsSizeX: window.innerWidth,
+      boundsSizeY: window.innerHeight,
     })
-
-    const cells = computed(() =>
-      genCells(
-        particlesData.positions,
-        particlesData.colors,
-        state.boundsSizeX,
-        state.boundsSizeY
-      )
-    )
+    const particles = ref(genParticles(state.boundsSizeX, state.boundsSizeY))
+    const cells = computed(() => genCells(particles.value, state.boundsSizeX, state.boundsSizeY))
 
     const { addAnimation } = useAnimationFrame()
 
-    addAnimation(({ deltaTime }) => {
-      if (deltaTime > 0.5) {
-        return
-      }
+    watchEffect(() => {
+      particles.value = genParticles(state.boundsSizeX, state.boundsSizeY)
+    })
 
-      for (let i = 0; i < state.particles.length; i++) {
-        const pos = state.particles[i].position
-        const vel = state.particles[i].velocity
+    addAnimation(() => {
+      if (Math.abs(window.innerWidth - state.boundsSizeX) > 100) {
+        state.boundsSizeX = window.innerWidth
+      }
+      if (Math.abs(window.innerHeight - state.boundsSizeY) > 100) {
+        state.boundsSizeY = window.innerHeight
+      }
+    })
+
+    addAnimation(({ deltaTime }) => {
+      for (let i = 0; i < particles.value.length; i++) {
+        const { position: pos, velocity: vel } = particles.value[i]
         pos[0] += vel[0] * deltaTime
         pos[1] += vel[1] * deltaTime
 
         if (pos[0] < 0) {
           pos[0] = 0
           vel[0] *= -1
-        } else if (pos[0] > boundsSizeX) {
-          pos[0] = boundsSizeX
+        } else if (pos[0] > state.boundsSizeX) {
+          pos[0] = state.boundsSizeX
           vel[0] *= -1
         }
 
         if (pos[1] < 0) {
           pos[1] = 0
           vel[1] *= -1
-        } else if (pos[1] > boundsSizeY) {
-          pos[1] = boundsSizeY
+        } else if (pos[1] > state.boundsSizeY) {
+          pos[1] = state.boundsSizeY
           vel[1] *= -1
         }
       }
@@ -157,7 +128,7 @@ export default {
     }
 
     const onCellHover = i => {
-      const particle = particles[i]
+      const particle = particles.value[i]
 
       if (particle.animation) {
         if (particle.animation.info.timeElapsed > 0.25) {
